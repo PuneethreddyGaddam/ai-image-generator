c4cdcea2b3e512dc05cba08eefe00ac46da945db
PuneethreddyGaddam <158322458+PuneethreddyGaddam@users.noreply.github.com>
2025-11-11 13:40:16 -0600

    fixing bugs

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 const app = express();
 
+
+
 function firstEndpoint(req, res) {
    res.send('server created');
 }
 
 app.get('/', firstEndpoint);
 
-app.post('/generate-image', (req, res) => {
+app.post('/generate-image', validationChecks, (req, res) => {
 
    console.log(req.body);
 })
