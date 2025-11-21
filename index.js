e3d29d4e8add02316c236915526811b63baf83ae
PuneethreddyGaddam <158322458+PuneethreddyGaddam@users.noreply.github.com>
2025-11-21 10:31:32 -0600

    added openai

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import corse from 'cors';
+import OpenAI from 'openai';
+import env from 'dotenv';
 
-
+env.config();
 const app = express();
+app.use(express.json());
 app.use(corse()); //app.confi, this is used to call middlewares
 
 
@@ -15,7 +18,38 @@ app.get('/', firstEndpoint);
 
 app.post('/generate-image', (req, res) => {
 
-   console.log(req.body);
+   const promptTxt = "Generate an image of an gray tabby cat hugging an otter with an orange scarf"
+   const imageObj = awaitgenerateImage(promptTxt);
+   console.log(imageObj);
+
+   
    res.send('connected to BE');
 })
-app.listen(4000);
+
+   async function generateImage(promptText){
+
+      const openai = new OpenAI({
+         apiKey: process.env.OPEN-AI-KEY,
+    });
+
+      try{
+          const response = await openai.images.generate({
+            model: "dall-e-3",
+            prompt: promptText,
+            n: 1,
+            size: "1024x1024",
+         });
+         return response.data[0].url;
+      }catch (error){
+         console.error("Error generating image:", error);
+         throw error;
+      }
+   }
+
+app.post('/generate-image', (req, res) => {
+   const openai = new OpenAI({
+      apiKey: process.env.OPEN-AI-KEY,
+   });
+   res.send('connected to BE');
+})
+app.listen(process.env.PORT);
